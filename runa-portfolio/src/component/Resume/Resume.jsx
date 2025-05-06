4349dc418456c05818abdc2ec3e490b31509bea1
Runa He <hejueru16@gmail.com>
2025-05-06 00:59:24 -0700

    build and deploy

--- runa-portfolio/src/component/Resume/Resume.jsx
+++ runa-portfolio/src/component/Resume/Resume.jsx
@@ -1,26 +1,3 @@
-// import React, { useEffect } from "react";
-// import "../../index.css";
-
-// function Resume() {
-//   useEffect(() => {
-//     window.open("/Portfolio/resume.pdf", "_blank", "noopener,noreferrer");
-//   }, []);
-
-//   return (
-//     <div style={{ textAlign: "center", padding: "100px 20px" }}>
-//       <h1>Redirecting to Resume...</h1>
-//       <p>
-//         If the PDF didn’t open,{" "}
-//         <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-//           click here
-//         </a>.
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default Resume;
-
 import React, { useEffect } from "react";
 import "../../index.css";
 
@@ -33,7 +10,7 @@ function Resume() {
     <div style={{ textAlign: "center", padding: "100px 20px" }}>
       <h1>Redirecting to Resume...</h1>
       <p>
-        If the PDF didn’t open,&nbsp;
+        If the PDF didn’t open,{" "}
         <a href="/Portfolio/resume.pdf" target="_blank" rel="noopener noreferrer">
           click here
         </a>.
@@ -43,4 +20,3 @@ function Resume() {
 }
 
 export default Resume;
-
