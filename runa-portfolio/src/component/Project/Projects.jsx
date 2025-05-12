9a4d33f17f8de5ef44c4ef8ed2d6fdde42cd22a6
Runa He <hejueru16@gmail.com>
2025-05-12 16:16:28 -0700

    fix the path

--- runa-portfolio/src/component/Project/Projects.jsx
+++ runa-portfolio/src/component/Project/Projects.jsx
@@ -11,7 +11,7 @@ const projectData = [
     "• Developed a music warm-up platform using HTML, JavaScript, CSS, and React, optimizing warm-up flows and increasing new user completion rates by 35% and engagement by 25%.\n" +
     "• Integrated Firebase to store user warm-up preferences, enabling personalized experiences and reducing setup time by 40%, significantly improving usability.\n" +
     "• Enhanced user interaction and responsive design; improved navigation structure to ensure smooth performance across devices (desktop, tablet, mobile), achieving a 90% user satisfaction rate.",
-    img: "/Portfolio/images/voxTune.jpg",
+    img: "/images/voxTune.jpg",
     ghLink: "https://github.com/runa-1117/VoxTune",
     demoLink: "https://voxtune-info340-wi25.web.app/",
     tags: ["HTML", "JavaScript", "CSS", "React", "FireBase"]
@@ -23,7 +23,7 @@ const projectData = [
     "• Researched 10+ existing solutions and optimized product design through iterative prototyping with 8 industry mentors.\n" +
     "• Developed a mobile app prototype with an interactive map and real-time tracking, increasing donor transparency by 60% and reducing mismatched donations by 40%.\n" +
     "• Awarded 2nd place among 70+ competing teams in the Best Overall Project category.",
-    img: "/Portfolio/images/needsNow.jpg",
+    img: "/images/needsNow.jpg",
     demoLink: "https://www.figma.com/proto/iCEO44VJEDPVpVmmn11eYP/WINFO-HACK---NeedsNow",
     tags: ["Figma", "UI/UX", "Interaction Design"]
   },
@@ -34,7 +34,7 @@ const projectData = [
     "• Led a 4-person team and designed Figma prototype addressing language barriers faced by older immigrants, achieving a 95% user satisfaction rate.\n" +
     "• Conducted user research with 10+ immigrant older adults, ensuring the app’s usability by integrating features like automatic translation and a photo gallery.\n" +
     "• Collaborated closely with stakeholders to continuously refine the design based on user feedback, resulting in a 40% increase in user engagement during pilot testing.",
-    img: "/Portfolio/images/photoLingo.jpg",
+    img: "/images/photoLingo.jpg",
     demoLink: "https://www.figma.com/design/OhFbZtbz14A4Sd8Yshic4X/INFO360-Prototype?node-id=1-3&p=f&t=GePDEMIKlxuwt7v0-0",
     tags: ["Figma", "UI/UX", "Prototyping", "Accessibility"]
   },
@@ -45,7 +45,7 @@ const projectData = [
     "• Led a team of 5 to design and prototype a badminton court reservation website, completing three iterations within 9 weeks and incorporating user feedback to refine functionality.\n" +
     "• Conducted research with 50+ students, applying user experience design principles to create personas that reflected user behaviors and needs.\n" +
     "• Earned the Best Design Award and a perfect project score due to the innovative and efficient solution.",
-    img: "/Portfolio/images/badminton.png",
+    img: "/images/badminton.png",
     demoLink: "https://gaojiayijerry.wixsite.com/imabadminton",
     tags: ["Web Design", "UI/UX", "Stakeholder Management"]
   }
